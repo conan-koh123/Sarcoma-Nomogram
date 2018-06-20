e158e3c80246bc1680fade4b513ee945eb798043
Koh Yen Sin <leafypix548@gmail.com>
2018-06-20 12:17:57 +0800

    Update R Code.R

--- R Code.R
+++ R Code.R
@@ -62,10 +62,10 @@ ui <- fluidPage(
       textOutput("DFS"), 
       br(), 
       br(),
-      p("Adapted from Outcome prediction in primary resected retroperitoneal soft tissue sarcoma: 
+      p("Adapted from 'Outcome prediction in primary resected retroperitoneal soft tissue sarcoma: 
         histology-specific overall survival and disease-free survival nomograms built on major sarcoma center data sets.
         Gronchi A, Miceli R, Shurell E, Eilber FC, Eilber FR, Anaya DA, Kattan MW, Honore C, Lev DC, Colobo C, Bonvalot S, 
-        Mariani L, Pollock RE. J Clin Oncol. 2013 May 1;21 (13): 1649-55. doi:10.1200/JCO.2012.44.3747. Epub 2013 Mar 25.")
+        Mariani L, Pollock RE. J Clin Oncol. 2013 May 1;21 (13): 1649-55. doi:10.1200/JCO.2012.44.3747. Epub 2013 Mar 25.'")
     )
   )
 )
